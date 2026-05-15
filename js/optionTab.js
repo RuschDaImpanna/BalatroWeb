7c63ec47744ddc0cc5424437fb71a454a59801c0
Rusch Da Impanna <ruschdaimpanna@gmail.com>
2026-05-15 09:44:06 -0500

    Make options tab

--- js/optionTab.js
+++ js/optionTab.js
@@ -4,11 +4,50 @@ optionBtn.addEventListener('click', () => {
 
     Swal.fire({
 
-        html: ``,
+        html: (`
+
+            <div class="menuOptSwal">
+
+                <ul id="menuList">
+
+                    <li>
+                    
+                        <button type="button" id="opt0Btn">Settings</button>
+
+                    </li>
+
+                    <li>
+                    
+                        <a href="deck.html"><button type="button" id="opt1Btn">New Run</button></a>
+
+                    </li>
+
+                    <li>
+                    
+                        <a href="main.html"><button type="button" id="opt2Btn">Main Menu</button></a>
+
+                    </li>
+
+                    <li>
+                    
+                        <button type="button" id="opt3Btn">Collection</button>
+
+                    </li>
+
+                </ul>
+
+            </div>
+            
+            `),
         showCloseButton: true,
         showConfirmButton: false,
+        showCancelButton: true,
+        cancelButtonText: 'Back',
+        cancelButtonColor: '#F3AD16',
         background: '#3C565E',
         didOpen: () => {
+
+            const listBtns = document.getElementById('menuList')
 
             requestAnimationFrame(() => {
 
